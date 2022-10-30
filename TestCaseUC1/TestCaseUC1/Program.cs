5e0a9bccc399fcc03cd4dcc2564ac5ac0ef5eae1
Sangmesh Methre <SangmeshMetre>
2022-10-30 17:10:03 +0530

    FindMax

---
+++ TestCaseUC1/TestCaseUC1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCaseUC1
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+            int output = Generics.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
