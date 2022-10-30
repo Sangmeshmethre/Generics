65d0bc4461a5f03c9d359ffee44366a78d3e8d78
Sangmesh Methre <SangmeshMetre>
2022-10-30 17:17:26 +0530

    FindMaximumNumber

---
+++ TestCaseUC2/TestCaseUC2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCaseUC2
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+            double output = Generics.MaximumFloatNumber(111.2, 22.0, 33.23);
+            Console.WriteLine(output);
+        }
+    }
+}
