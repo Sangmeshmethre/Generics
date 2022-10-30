424cab7b1ccbbdab76007b8e5319e8d7e5c4491a
Sangmesh Methre <SangmeshMetre>
2022-10-30 17:01:59 +0530

    Generics Find Max

---
+++ BasicProgarm195TC/BasicProgarm195TC/PrintArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgarm195TC
+{
+    public class PrintArray<T>
+    {
+        private T[] inputArray;
+        public PrintArray(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void toPrint()
+        {
+            //Console.WriteLine(this.inputArray);
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine(".....................................");
+        }
+    }
+}
---
+++ BasicProgarm195TC/BasicProgarm195TC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgarm195TC
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 4, 5, 6, 7 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+            char[] charArray = { 'S', 'A', 'N', 'G', 'M', 'E', 'S', 'H' };
+
+            new PrintArray<int>(intArray).toPrint();
+            new PrintArray<double>(doubleArray).toPrint();
+            new PrintArray<char>(charArray).toPrint();
+        }
+    }
+}
