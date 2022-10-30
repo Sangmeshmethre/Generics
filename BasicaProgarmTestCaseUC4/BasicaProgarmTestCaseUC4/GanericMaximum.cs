5c98fbc7dab82897cc9e8c221007ab0c52b261fd
Sangmesh Methre <SangmeshMetre>
2022-10-30 17:32:00 +0530

    FindMaximumNumber

---
+++ BasicaProgarmTestCaseUC4/BasicaProgarmTestCaseUC4/GanericMaximum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicaProgarmTestCaseUC4
+{
+    public  class GanericMaximum<T> where T : IComparable
+    {
+        public T[] value;
+        public GanericMaximum(T[] value)
+        {
+            this.value = value;
+        }   
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_value = Sort(value);
+            return sorted_value[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum Value is " + max);
+        }
+    }
+}
---
+++ BasicaProgarmTestCaseUC4/BasicaProgarmTestCaseUC4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicaProgarmTestCaseUC4
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 112, 252, 585, 666, 857 };
+            GanericMaximum<int>ganeric = new GanericMaximum<int>(intArray);
+            ganeric.PrintMaxValue();
+
+            double[] doubleArray = { 118.25, 252.24, 585.458, 666.54, 457.457 };
+            GanericMaximum<double> ganeric1 = new GanericMaximum<double>(doubleArray);
+            ganeric1.PrintMaxValue();
+
+            string[] stringArray = { "S", "A", "N", "G", "M", "E", "S", "H" };
+            GanericMaximum<string> ganeric3 = new GanericMaximum<string>(stringArray);
+            ganeric3.PrintMaxValue();
+
+
+        }
+    }
+}
