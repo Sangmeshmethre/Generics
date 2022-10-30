e33fbcea7a02016314032c05c5bb275940f86d41
Sangmesh Methre <SangmeshMetre>
2022-10-30 16:40:11 +0530

    Generics Program

---
+++ BasicProgarm195UC1Gen/Generis.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgarm195UC1Gen
+{
+    public class Generis
+    {
+        public static void toPrinrt(int[] inputArray)
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("............................");
+        }
+        public static void toPrinrt(double[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("............................");
+        }
+        public static void toPrinrt(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("............................");
+        }
+    }
+}
---
+++ BasicProgarm195UC1Gen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgarm195UC1Gen
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            char[] charArray = { 'S', 'A', 'N', 'G', 'M', 'E', 'S', 'H' };
+
+            Generis.toPrinrt(intArray);
+            Generis.toPrinrt(doubleArray);
+            Generis.toPrinrt(charArray);
+        }
+    }
+}
