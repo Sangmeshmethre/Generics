b8af6f5edda0ee7b90ba954f521096013b060c9a
Sangmesh Methre <SangmeshMetre>
2022-10-31 23:31:44 +0530

    Refrectar Program

---
+++ BasicaProgarm195RF/BasicaProgarm195RF/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicaProgarm195RF
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            //int[] intArray = { 112, 344, 432, 555, 678 };
+            GemericMaximum<int> generic = new GemericMaximum<int>(2, 3, 1);
+            int result = generic.MaxMethod();
+            Console.WriteLine(result);
+
+
+            //double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
+            GemericMaximum<double> generic1 = new GemericMaximum<double>(21.2, 42.2, 53.2);
+            double result1 = generic1.MaxMethod();
+            Console.WriteLine(result1);
+        }
+    }
+}
